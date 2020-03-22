cd6ccdad9cfdb1d5b40cf1e6313936f7f1babdad
bekabot <87282211776dom>
2020-03-22 14:16:29 +0600

    auth activity setup

---
+++ app/src/main/java/kz/iitu/iituboardandroid/App.kt
@@ -0,0 +1,38 @@
+package kz.iitu.iituboardandroid
+
+import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.multidex.BuildConfig
+import androidx.multidex.MultiDex
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@App)
+            modules(
+                listOf(
+                )
+            )
+        }
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
+}
---
+++ app/src/main/java/kz/iitu/iituboardandroid/ui/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package kz.iitu.iituboardandroid.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kz.iitu.iituboardandroid.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
