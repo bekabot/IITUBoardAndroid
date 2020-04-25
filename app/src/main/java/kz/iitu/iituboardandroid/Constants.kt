225c5b0d0cc2865c6b888133d4894404843027c6
bekabot <87282211776dom>
2020-04-25 14:26:26 +0600

    remove news filters

--- app/src/main/java/kz/iitu/iituboardandroid/Constants.kt
+++ app/src/main/java/kz/iitu/iituboardandroid/Constants.kt
@@ -1,8 +1,8 @@
 package kz.iitu.iituboardandroid
 
 object Constants {
-//    const val HOST = "http://10.0.2.2:8000"
-        const val HOST = "https://iitu-board.herokuapp.com"
+    const val HOST = "http://10.0.2.2:8000"
+//        const val HOST = "https://iitu-board.herokuapp.com"
     const val REMEMBER_ME = "REMEMBER_ME"
     const val EXTRA_RECORD_ID = "record_id"
     const val REQUEST_CODE_OPEN_IMAGE_PICKER = 122
